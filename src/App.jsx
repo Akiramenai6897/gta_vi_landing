a4404806676bc79cd62729ed380d3fa5d949f703
Haider <haider.sm6897@yahoo.com>
2025-07-16 21:38:34 +0500

    Implement First Video

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import Hero from "./sections/Hero.jsx";
 import Navbar from "./sections/Navbar.jsx";
+import FirstVideo from "./sections/FirstVideo.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -11,6 +12,7 @@ const app = () => {
         <main>
             <Navbar />
             <Hero />
+            <FirstVideo />
         </main>
     );
 };
